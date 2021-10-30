981a83d598c67fd3d44e73011dd55087fefaac3c
Chun Huang <chunzhuang607@gmail.com>
2021-10-30 15:32:41 -0700

    adding game command history

---
+++ HtH-Practice/variables-and-datatypes.py
@@ -0,0 +1,8 @@
+print ("hello world")
+my_text="Hello World"
+print(my_text)
+print ("I drink", glass_of_water, "glasses of water today")
+glass_of_water=3
+glass_of_water=glass_of_water + 1
+print()
+
